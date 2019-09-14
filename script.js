226fe4f36107c9b31c8be61180b695d22d8b6bc6
rjbx <bob@coded.art>
2019-09-13 23:37:48 -0400

    Style body elements

--- script.js
+++ script.js
@@ -1,70 +1,79 @@
+const inputStyle = { width: '235px', height: '30px', backgroundColor: '#449944', fontFamily: 'Quantico', fontSize: '20px', textAlign: 'right' };
+const mainStyle = { margin: 'auto', width: '260px', height: '360px', verticalAlign: 'middle', border: '5px groove #0090F7', borderRadius: '0px 0px 15px 15px', backgroundColor: '#0070D7' };
+const titleStyle = { margin: '10px', fontFamily: 'Quantico', fontWeight: 'bold', fontStyle: 'italic', fontSize: '16px', color: 'white', textShadow: '1px 1px #222' };
+const redBtnStyle = { width: '60px', height: '24px', backgroundColor: '#C73000', color: 'white', fontWeight: 'bold', fontSize: '11px' };
+const blueBtnStyle = { width: '60px', height: '24px', backgroundColor: 'white', color: '#1040A0', fontWeight: 'bold', fontSize: '11px' };
 const Btn = (props) =>
-React.createElement("button", { style: { width: '60px' }, onClick: props.handleClick, value: props.text }, props.text);
-const Title = () =>
-React.createElement("center", null, React.createElement("h1", null, "Calculator"));
+React.createElement("button", { style: props.style, onClick: props.handleClick, value: props.text }, props.text);
+const Title = () => React.createElement("h3", { style: titleStyle }, "TG-108");
 const Attribution = () =>
-React.createElement("center", null, React.createElement("h5", null, "Made by rjbx with React and MathJS"));
+React.createElement("center", null, React.createElement("h5", { style: { fontFamily: 'sans', fontSize: '8px' } }, "Made by rjbx with React and MathJS"));
 const Display = (props) =>
-React.createElement("table", { style: { margin: 'auto' }, onClick: props.handleClick },
-React.createElement("input", { style: { width: '235px' }, value: props.expr, placeholder: props.temp, onChange: props.handleChange, onClick: props.handleClick }));
+React.createElement("table", { style: { margin: 'auto', paddingTop: '20px' }, onClick: props.handleClick },
+React.createElement("input", { style: inputStyle, value: props.expr, placeholder: props.temp, onChange: props.handleChange, onClick: props.handleClick }));
 
 const Keypad = (props) =>
 React.createElement("table", { style: { margin: 'auto' } },
 React.createElement("tbody", null,
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "del" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "(" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: ")" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "=" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "del", style: redBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "(", style: redBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: ")", style: redBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "=", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "9" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "8" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "7" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "+" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "9", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "8", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "7", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "+", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "6" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "5" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "4" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "\u2013" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "6", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "5", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "4", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "\u2013", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "3" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "2" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "1" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "*" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "3", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "2", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "1", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "*", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "0" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "." }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "^" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "/" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "0", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: ".", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "^", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "/", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "log" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "ln" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "e^" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "sqrt" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "log", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "ln", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "e^", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "sqrt", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "sin" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "cos" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "tan" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "pi" })),
+React.createElement(Btn, { handleClick: props.handleClick, text: "sin", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "cos", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "tan", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "pi", style: redBtnStyle })),
 
 React.createElement("tr", null,
-React.createElement(Btn, { handleClick: props.handleClick, text: "arcsin" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "arccos" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "arctan" }),
-React.createElement(Btn, { handleClick: props.handleClick, text: "deg" }))));
+React.createElement(Btn, { handleClick: props.handleClick, text: "arcsin", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "arccos", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "arctan", style: blueBtnStyle }),
+React.createElement(Btn, { handleClick: props.handleClick, text: "deg", style: redBtnStyle }))));
 
 
 
-/* 'solar panel' switch for activation in dark mode */
-const Panel = () =>
-React.createElement("table", { style: { margin: 'auto', border: '1px solid grey', width: '240px', height: '20px' } },
-React.createElement("tr", null));
+const Panel = (props) =>
+React.createElement("button", { value: "panel", style: { backgroundColor: '#0070D7', border: '3px inset #0090FF' }, onClick: props.handleClick },
+React.createElement("table", { style: { margin: 'auto', border: '1px solid #952', width: '240px', height: '30px', backgroundColor: '#222' } },
+React.createElement("tr", null,
+React.createElement("td", { style: { border: '1px solid #520' } }),
+React.createElement("td", { style: { border: '1px solid #520' } }),
+React.createElement("td", { style: { border: '1px solid #520' } }),
+React.createElement("td", { style: { border: '1px solid #520' } }))));
+
 
 
 
@@ -82,9 +91,10 @@ class Main extends React.Component {
   }
   render() {
     return (
-      React.createElement("main", null,
-      React.createElement(Panel, null),
+      React.createElement("main", { style: mainStyle },
       React.createElement(Display, { expr: this.props.expr, temp: this.props.temp, handleChange: this.props.handleChange }),
+      React.createElement(Header, null),
+      React.createElement(Panel, { handleClick: this.props.handleClick }),
       React.createElement(Keypad, { handleClick: this.props.handleClick })));
 
 
@@ -104,15 +114,23 @@ class Calculator extends React.Component {
     super(props);
     this.state = {
       expr: '',
-      temp: '0' };
+      temp: '0',
+      mode: false };
+
+    WebFont.load({
+      google: {
+        families: ['Quantico:400,700i'] } });
+
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.toggleMode = this.toggleMode(this);
   }
   handleClick(event) {
     let value = event.target.value;
     console.log(event.target.value);
     switch (value) {
+      case 'panel':this.toggleMode();break;
       case '=':this.handleEval();break;
       case 'del':this.handleInput(this.state.expr.slice(0, -1));break;
       default:this.handleInput(this.state.expr + value);}
@@ -130,14 +148,18 @@ class Calculator extends React.Component {
     let result = math.evaluate(this.state.expr);
     this.setState({
       expr: '',
-      temp: result,
-      dark: false });
+      temp: result });
+
+  }
+  toggleMode() {
+    let inverse = !this.state.dark;
+    this.setState({
+      mode: inverse });
 
   }
   render() {
     return (
       React.createElement("body", null,
-      React.createElement(Header, null),
       React.createElement(Main, { expr: this.state.expr, temp: this.state.temp, handleChange: this.handleChange, handleClick: this.handleClick }),
       React.createElement(Footer, null)));
 
